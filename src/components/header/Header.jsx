53d832c8b07cbdf3b08e346e38ffdfa68f57a896
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-08-08 22:22:06 +0500

    react005

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const Header = () => {
+    const [count, setCount] = useState(10)
+  return (
+    <div>
+        <h2>Heade</h2>
+    </div>
+  )
+}
+
+export default Header
