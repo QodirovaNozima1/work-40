2db2e075bd2084dba7a3d795439f542bc2c44aa1
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-08-09 07:45:03 +0500

    react005

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,40 @@
-import React, {useState} from 'react'
-
+import React, { useState } from 'react'
+import "./header.css"
+import navlogo from "../../assets/click.png"
+import { AiOutlineMenuUnfold } from "react-icons/ai";
 const Header = () => {
-    const [count, setCount] = useState(10)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const links = ["Каталог", "Доставка", " Условия", "Контакты"]
+  const number = ["+ 375 736 463 64 72  /+ 375 736 463 64 72"]
   return (
-    <div>
-        <h2>Heade</h2>
+    <div className="header bg-slate-200 mb-32">
+      <div className='container w-5/6'>
+        <nav className="navbar py-5 flex items-center justify-between">
+          <div className="navbar__logo">
+            <img src={navlogo} alt="" />
+          </div>
+          <div className={`nav__collect ${isMenuOpen ? "show" : ""}`}>
+            <ul className="navbar__collection flex gap-7">
+              {
+                links?.map((el, index) => (<li key={index} className="navbar__item"><a href="#"><span>{el}</span></a></li>))
+              }
+            </ul>
+          </div>
+          <div className="navbar__number  flex-col text-base font-medium ">
+            {
+              number?.map((el, index) => (<li key={index} className="navbar__item list-none"><a href="#"><span>{el}</span></a></li>))
+            }
+            <p className='text-end font-extralight text-sm'>Заказать звонок</p>
+          </div>
+          <div onClick={toggleMenu} className="navbar__menu text-2xl">
+          <AiOutlineMenuUnfold />
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
