00c97072f49523dda3ee1ee89e6dd4659d9da9ff
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-08-09 07:56:16 +0500

    finish

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./footer.css"
+import "./Footer.css"
 import navlogo from "../../assets/click.png"
 import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
